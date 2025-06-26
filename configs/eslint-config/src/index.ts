89138405057c43374e8f9a3b4a1687a2fd520e94
AnthonyLuo1998 <jum1274001055@gmail.com>
2025-06-26 16:18:04 +0800

    feat: hooks,eslint

--- configs/eslint-config/src/index.ts
+++ configs/eslint-config/src/index.ts
@@ -2,11 +2,10 @@
  * @Author: AnthonyLuo
  * @Date: 2025-06-26 01:09:37
  * @LastEditors: AnthonyLuo
- * @LastEditTime: 2025-06-26 02:37:29
+ * @LastEditTime: 2025-06-26 15:49:28
  * @Email: jum1274001055@gmail.com
  */
 import antfu from '@antfu/eslint-config'
-import tailwindcss from 'eslint-plugin-tailwindcss'
 
 export default antfu(
   {
@@ -26,5 +25,4 @@ export default antfu(
       'no-console': 'warn',
     },
   },
-  ...tailwindcss.configs['flat/recommended'],
 )
