be9833a08cddda10fa183c516161451909d2f52d
AnthonyLuo1998 <jum1274001055@gmail.com>
2025-06-26 15:38:05 +0800

    test: husky hook

--- apps/pro-app/src/app.vue
+++ apps/pro-app/src/app.vue
@@ -2,12 +2,12 @@
  * @Author: AnthonyLuo
  * @Date: 2025-06-25 16:46:08
  * @LastEditors: AnthonyLuo
- * @LastEditTime: 2025-06-26 15:34:41
+ * @LastEditTime: 2025-06-26 15:37:30
  * @Email: jum1274001055@gmail.com
 -->
 
 <script setup lang="ts">
-console.log('Hello World');
+  console.log('Hello World');;;;;;;
 </script>
 
 <template>
