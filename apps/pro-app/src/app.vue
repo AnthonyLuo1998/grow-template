b08c10e75dea447969401ad7ec115a833044d9bf
AnthonyLuo1998 <jum1274001055@gmail.com>
2025-06-26 15:33:36 +0800

    feat: 添加 Husky 钩子脚本以增强 Git 提交和推送流程

--- apps/pro-app/src/app.vue
+++ apps/pro-app/src/app.vue
@@ -2,12 +2,11 @@
  * @Author: AnthonyLuo
  * @Date: 2025-06-25 16:46:08
  * @LastEditors: AnthonyLuo
- * @LastEditTime: 2025-06-26 01:56:44
+ * @LastEditTime: 2025-06-26 02:51:08
  * @Email: jum1274001055@gmail.com
 -->
 
 <script setup lang="ts">
-console.log(123)
 </script>
 
 <template>
